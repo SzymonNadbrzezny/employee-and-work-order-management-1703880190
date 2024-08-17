275fc19d9287ca98ab282b245e83e380db6fc231
Szymon Nadbrzezny <gludekpl@gmail.com>
2024-08-17 17:11:23 +0000

    feat:

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,10 @@ Rails.application.configure do
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :resend
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default charset: "utf-8"
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
